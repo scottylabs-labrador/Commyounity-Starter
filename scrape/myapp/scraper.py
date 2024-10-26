62bf72f7cc69292cf547c0ddad335347fc259743
Kileb016 <tanat5342@gmail.com>
2024-10-26 02:07:18 -0400

    Parsed Top 3 Entries

--- scrape/myapp/scraper.py
+++ scrape/myapp/scraper.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options 
 from bs4 import BeautifulSoup
-from myapp.models import Events
+# from myapp.models import Events
 import time
 
 class Event_Container:
@@ -21,37 +21,45 @@ def scrape():
     service = Service(edge_driver_path)
     options = Options()
     options.add_argument('--lang=en-US')
-    driver = webdriver.Edge(service=service, options=options)
-    driver.get('https://www.bing.com/search?q=All+Events+Events+in+Philadelphia&filters=latlong%3a%2239.9510612487793%2c-75.16561889648438%22+location%3a%22Philadelphia%22+eventcity%3a%22020d4bbf-2971-4236-b87d-c3ec1d7f851c%22+catesegtype%3a%22ZXZlbnRfY2l0eQ%3d%3d%22+eventsgroup%3a%22MTQw%22+date%3a%2220241005_20241231%22+PopulatedPlaceGeoID%3a%22YWZlNTQzZmYtNzFlMi00ZWQ0LWFjMjAtNDk0ZjQ5MjI3MTM3Iw%3d%3d%22+GeoIds%3a%22YWZlNTQzZmYtNzFlMi00ZWQ0LWFjMjAtNDk0ZjQ5MjI3MTM3JHBvcHVsYXRlZHBsYWNlIw%3d%3d%22+mltype%3a%221%22+eltypedim1%3a%22Event%22+secq%3a%22All+Events+Events+in+Philadelphia%22+tsource%3a%22events%22+supwlcar%3a%221%22+eventdg%3a%22false%22+segment%3a%22generic.carousel%22+ctype%3a%224%22+UserId%3a%22E2668640D09481C26C4D615EFFFFFFFF%22')
+    # driver = webdriver.Edge(service=service, options=options)
+    driver = webdriver.Chrome()
+
+    driver.get('https://www.google.com/search?q=pittsburgh+events&sca_esv=ea89727b02e8330d&ei=V7QBZ8r5AeCs5NoPvO-GiAk&ved=0ahUKEwiKgZSQnPiIAxVgFlkFHby3AZEQ4dUDCA8&uact=5&oq=pittsburgh+events&gs_lp=Egxnd3Mtd2l6LXNlcnAiEXBpdHRzYnVyZ2ggZXZlbnRzMgoQABiwAxjWBBhHMgoQABiwAxjWBBhHMgoQABiwAxjWBBhHMgoQABiwAxjWBBhHMgoQABiwAxjWBBhHMgoQABiwAxjWBBhHMgoQABiwAxjWBBhHMgoQABiwAxjWBBhHMg0QABiABBiwAxhDGIoFMg0QABiABBiwAxhDGIoFMg0QABiABBiwAxhDGIoFMg0QABiABBiwAxhDGIoFMhwQLhiABBiwAxhDGMcBGMgDGIoFGI4FGK8B2AEBMhwQLhiABBiwAxhDGMcBGMgDGIoFGI4FGK8B2AEBMhkQLhiABBiwAxjRAxhDGMcBGMgDGIoF2AEBMhwQLhiABBiwAxhDGMcBGMgDGIoFGI4FGK8B2AEBMhkQLhiABBiwAxhDGMcBGMgDGIoFGK8B2AEBMhwQLhiABBiwAxhDGMcBGMgDGIoFGI4FGK8B2AEBMhwQLhiABBiwAxhDGMcBGMgDGIoFGI4FGK8B2AEBMhMQLhiABBiwAxhDGMgDGIoF2AEBSKoLUL0HWP8JcAF4AJABAJgBXaAB7AKqAQE1uAEDyAEA-AEBmAIFoAKxAsICDRAAGIAEGLEDGIMBGA3CAgcQABiABBgNwgIQEAAYgAQYsQMYgwEYigUYDcICEBAAGIAEGLEDGEMYgwEYigXCAg0QABiABBixAxiDARgKwgIHEAAYgAQYCsICEBAAGIAEGLEDGIMBGIoFGAqYAwDiAwUSATEgQIgGAZAGFLoGBggBEAEYCJIHATWgB-9b&sclient=gws-wiz-serp')
     time.sleep(5)
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    ol = soup.find('ol', class_='items')
-    a_elements = ol.find_all('a')
+    # ol = soup.find_all('div', attrs={'jscontroller':'NGvQ3'})
+    # ol = soup.find_all('li', attrs={'class':'PaEvOc'})
+    ol = soup.find_all('div', attrs={'class':'fzHfmf'})
+
+
+    # a_elements = ol.find_all('a')
     data_list = []
 
     #scrape
-    for a in a_elements:
-        tit = a.find("span", class_="tit").get_text(strip=True)
-        cat = a.find("div", class_ = "evtcat").get_text(strip=True)
-        b_factrows = a.find_all("div", class_="b_factrow")
-        loc = b_factrows[1].get_text(strip=True)
-        tim = b_factrows[2].get_text(strip=True)
-        float_meta = a.find("div", class_="b_float_meta")
-        m = float_meta.contents[0].get_text(strip=True)
-        meta_divs = float_meta.find_all("div")
-        d = meta_divs[0].get_text(strip=True)
-        w = meta_divs[1].get_text(strip=True)
+    for a in ol:
+        tit = a.find("div", class_="YOGjf").get_text(strip=True)
+        temp = a.find("div", class_ = "cEZxRc").get_text(strip=True)
+        tim = ''.join([i if ord(i) < 128 else ' ' for i in temp]).replace(",","").split()
+        [k, j ]= a.find_all("div", class_ = "cEZxRc zvDXNd")
+        [name,place] =[k.get_text(strip=True), j.get_text(strip=True)]
+
+
         
         data = Event_Container();
         data.title = tit;
-        data.category = cat;
-        data.location = loc;
+        # data.category = cat;
+        # data.location = loc;
         data.time = tim;
+
+        m = 0
+        d = 0
+        if(temp[0] == "Today"):
+            m = 14
         data.month = m;
         data.day = d;
-        data.weekday = w;
+
         data_list.append(data);
 
     driver.quit()
@@ -60,13 +68,15 @@ def scrape():
 def insert():
     datalist = scrape();
     for event in datalist:
-        Events.objects.create(
-            title=event.title,
-            category=event.category,
-            location=event.location,
-            month=event.month,
-            day=event.day,
-            weekday=event.weekday,
-            time=event.time,
-        )
+        print("Title: " +event.title + "Category: " +event.category + "Location: "+event.location + "Month: "+event.month+"Day: "+event.day+"Weekday: "+event.weekday+"Time: "+event.time)
+        # Events.objects.create(
+        #     title=event.title,
+        #     category=event.category,
+        #     location=event.location,
+        #     month=event.month,
+        #     day=event.day,
+        #     weekday=event.weekday,
+        #     time=event.time,
+        # )
 
+insert()
